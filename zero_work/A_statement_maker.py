5d6cf38a60588256bba2414cd5595782ee64080d
JollyWaverley <150859838+JollyWaverley@users.noreply.github.com>
2025-02-05 14:34:51 +1300

    Add files via upload

---
+++ zero_work/A_statement_maker.py
@@ -0,0 +1,8 @@
+# function here
+def generate_statement(statement, decoration):
+    """makes the statement pop with added emojis"""
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main routine here
+generate_statement("programing is fun", "👌")
