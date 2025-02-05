8a36874757ef227b65b7a928afc61aa0a1e1b2b0
JollyWaverley <150859838+JollyWaverley@users.noreply.github.com>
2025-02-05 15:00:38 +1300

    Add files via upload

---
+++ A_statement_maker.py
@@ -0,0 +1,24 @@
+# function here
+def generate_statement(statement, decoration, lines):
+    """will make the headings (3 lines), subhaedings(2 lines) and emphasised text / mini-heading (1 line).
+       Only use emoji for single line statements"""
+
+    middle=(f"{decoration * 3} {statement} {decoration * 3}")
+    top_bottem = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottem)
+
+    else:
+        print(top_bottem)
+        print(middle)
+        print(top_bottem)
+
+
+# Main routine here
+generate_statement("programing is fun", "=",3)
+generate_statement("programing is still fun" , "-",2)
+generate_statement("emoji in action","😎",1)
