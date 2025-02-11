38402bffd62223078b654fc8122255bfec50ed34
JollyWaverley <150859838+JollyWaverley@users.noreply.github.com>
2025-02-12 10:30:12 +1300

    yes no checker

---
+++ yes_no.py
@@ -0,0 +1,25 @@
+# function here
+def yes_no(question):
+
+
+    while True:
+        responce = input(question).lower()
+
+
+        if responce == "yes" or responce == "y":
+            return "yes"
+        if responce == "no" or responce == "n":
+            return "no"
+
+        else:print("please enter yes or no")
+
+#main route here
+
+#loop for testing
+while True:
+    want_instruction = yes_no("would you like to read the instructions? ")
+    print(f"you chose {want_instruction}\n")
+
+
+
+
